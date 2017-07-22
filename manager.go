ce7b9ce6270620f4b97dfc3f05e824793d2dfeb0
Sietse Ringers <mail@sietseringers.net>
2017-07-22 15:04:44 +0200

    Remove test functions

--- manager.go
+++ manager.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"io/ioutil"
 	"math/big"
-	"net/http"
 	"os"
 
 	"github.com/mhe/gabi"
@@ -97,44 +96,4 @@ func (cm *CredentialManager) init(path string) (err error) {
 	cm.secretkey = new(big.Int).SetBytes(bytes)
 
 	return
-}
-
-func Test2(path string) (content string, err error) {
-	bytes, err := ioutil.ReadFile(path + "/file.txt")
-	content = string(bytes)
-	return
-}
-
-func Test(path string) (err error) {
-	err = ioutil.WriteFile(path+"/file.txt", []byte("TEST TEST"), 0755)
-	return
-}
-
-type NetworkHandler interface {
-	Success(content string)
-	Error(status int, error string)
-}
-
-func TestNetwork(url string, handler NetworkHandler) {
-	go func() {
-		resp, err := http.Get(url)
-		if err != nil {
-			handler.Error(0, err.Error())
-			return
-		}
-		defer resp.Body.Close()
-
-		bytes, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			handler.Error(0, err.Error())
-			return
-		}
-
-		content := string(bytes)
-		if resp.StatusCode < 200 || resp.StatusCode > 208 {
-			handler.Error(resp.StatusCode, content)
-		} else {
-			handler.Success(content)
-		}
-	}()
 }
