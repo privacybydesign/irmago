844380172620d04c4c968e392d5c2afa6123b558
Sietse Ringers <mail@sietseringers.net>
2017-07-26 16:05:22 +0200

    Update package doc

--- doc.go
+++ doc.go
@@ -1,4 +1,4 @@
-// Work in progress on an IRMA client in Go. It will:
+// Package irmago is work in progress on an IRMA client in Go. It will:
 //* (De)serialize credentials from/to storage
 //* Be the client (like the IRMA Android app) in the IRMA protocol
 //  see (https://credentials.github.io/protocols/irma-protocol/).
