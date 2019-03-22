556e6b6c1044e438649f526079e537a3d7796a86
Sietse Ringers <mail@sietseringers.net>
2019-03-22 21:20:19 +0100

    Make bolthold an explicit dependency

--- doc.go
+++ doc.go
@@ -2,3 +2,7 @@
 // It parses irma_configuration folders to scheme managers, issuers, credential types and public keys,
 // it contains various messages from the IRMA protocol, and parses IRMA metadata attributes.
 package irma
+
+import "github.com/timshannon/bolthold"
+
+var _ = bolthold.Key
