82a4ad471bf7854b0e30215cddbfeb1591853ff1
Confiks <confiks@scriptbase.org>
2019-09-10 12:04:55 +0200

    feat: Add IssuanceTime to DisclosuedAttribute struct (for 'de vita' attestation case)

--- verify.go
+++ verify.go
@@ -31,10 +31,11 @@ const (
 
 // DisclosedAttribute represents a disclosed attribute.
 type DisclosedAttribute struct {
-	RawValue   *string                 `json:"rawvalue"`
-	Value      TranslatedString        `json:"value"` // Value of the disclosed attribute
-	Identifier AttributeTypeIdentifier `json:"id"`
-	Status     AttributeProofStatus    `json:"status"`
+	RawValue     *string                 `json:"rawvalue"`
+	Value        TranslatedString        `json:"value"` // Value of the disclosed attribute
+	Identifier   AttributeTypeIdentifier `json:"id"`
+	Status       AttributeProofStatus    `json:"status"`
+	IssuanceTime Timestamp               `json:"issuancetime"`
 }
 
 // ProofList is a gabi.ProofList with some extra methods.
@@ -243,10 +244,11 @@ func parseAttribute(index int, metadata *MetadataAttribute, attr *big.Int) (*Dis
 		status = AttributeProofStatusNull
 	}
 	return &DisclosedAttribute{
-		Identifier: attrid,
-		RawValue:   attrval,
-		Value:      NewTranslatedString(attrval),
-		Status:     status,
+		Identifier:   attrid,
+		RawValue:     attrval,
+		Value:        NewTranslatedString(attrval),
+		Status:       status,
+		IssuanceTime: Timestamp(metadata.SigningDate()),
 	}, attrval, nil
 }
 
