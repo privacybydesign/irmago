e472c721320ec0f88de8a595e54dc767f66be63b
Rowan Goemans <rowan.goemans@bdtnl.com>
2019-12-18 13:53:39 +0100

    Added start QR scanner on startup preference.

--- irmaclient/client.go
+++ irmaclient/client.go
@@ -62,10 +62,12 @@ var SentryDSN = ""
 
 type Preferences struct {
 	EnableCrashReporting bool
+	QrScannerOnStartup bool
 }
 
 var defaultPreferences = Preferences{
 	EnableCrashReporting: true,
+	QrScannerOnStartup: false,
 }
 
 // KeyshareHandler is used for asking the user for his email address and PIN,
@@ -985,6 +987,13 @@ func (client *Client) LoadLogsBefore(beforeIndex uint64, max int) ([]*LogEntry, 
 // Has effect only after restarting.
 func (client *Client) SetCrashReportingPreference(enable bool) {
 	client.Preferences.EnableCrashReporting = enable
+	_ = client.storage.StorePreferences(client.Preferences)
+	client.applyPreferences()
+}
+
+// SetQrScannerOnStartup toggles whether or not QR scanner is shown on startup of app.
+func (client *Client) SetQrScannerOnStartupPreference(enable bool) {
+	client.Preferences.QrScannerOnStartup = enable
 	_ = client.storage.StorePreferences(client.Preferences)
 	client.applyPreferences()
 }
