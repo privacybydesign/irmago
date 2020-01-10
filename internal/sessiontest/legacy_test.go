86f6d9a92dbb5f63a9d176414a6e98490f14fd53
Ivar Derksen <ivarderksen@hotmail.nl>
2020-01-10 16:40:39 +0100

    Added legacy storage tests for internal/sessiontest

---
+++ internal/sessiontest/legacy_test.go
@@ -0,0 +1,27 @@
+package sessiontest
+
+import (
+	irma "github.com/privacybydesign/irmago"
+	"github.com/privacybydesign/irmago/internal/test"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestSessionUsingLegacyStorage(t *testing.T) {
+	test.SetTestStorageDir("legacy_teststorage")
+	client, _ := parseStorage(t)
+
+	// Issue new credential
+	sessionHelper(t, getMultipleIssuanceRequest(), "issue", client)
+
+	// Close client to prevent database to be opened twice
+	err := client.Close()
+	require.NoError(t, err)
+
+	// Test whether credential is still there
+	require.NoError(t, err)
+	id := irma.NewAttributeTypeIdentifier("irma-demo.MijnOverheid.root.BSN")
+	sessionHelper(t, getDisclosureRequest(id), "verification", client)
+
+	test.SetTestStorageDir("teststorage")
+}
