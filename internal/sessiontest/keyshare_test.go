53bfee93caac69341ba8a73e219a3391aab3872e
David Venhoek <d.venhoek@privacybydesign.foundation>
2020-01-30 15:55:30 +0100

    Changed sessiontest/keyshare_test to run against new keyshare server.

--- internal/sessiontest/keyshare_test.go
+++ internal/sessiontest/keyshare_test.go
@@ -1,17 +1,79 @@
-// +build !local_tests
-
 package sessiontest
 
 import (
+	"context"
+	"encoding/base64"
+	"net/http"
+	"path/filepath"
 	"testing"
 
-	"github.com/privacybydesign/irmago"
+	"github.com/go-chi/chi"
+	"github.com/privacybydesign/irmago/internal/keysharecore"
+
+	irma "github.com/privacybydesign/irmago"
 	"github.com/privacybydesign/irmago/internal/test"
 	"github.com/privacybydesign/irmago/irmaclient"
+	"github.com/privacybydesign/irmago/server/keyshareserver"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
+var serv *http.Server
+
+func startKeyshareServer(t *testing.T) {
+	db := keyshareserver.NewMemoryDatabase()
+	db.NewUser(keyshareserver.KeyshareUserData{
+		Username: "",
+		Coredata: keysharecore.EncryptedKeysharePacket{},
+	})
+	var ep keysharecore.EncryptedKeysharePacket
+	p, err := base64.StdEncoding.DecodeString("YWJjZB7irkDzwMWtBC6PTItWmO2AgAGm1/gFOyrd+nyt3/0GaHLY5Z1S1TM6N5nzb1Jh+Nqx0z0c3f9R2UyoYuy+pnrerTpYL1mpoZZfz8MPqcrAMsmVdb2kHH0BuAGSC0V28tp1BCVzhYnfMJyrUlNWonsTWSn68Av1BwpIBOGxqBXYfW0JzaffuSmZIyubImmTN7p32ASbseJSNwu0Rg==")
+	require.NoError(t, err)
+	copy(ep[:], p)
+	db.NewUser(keyshareserver.KeyshareUserData{
+		Username: "testusername",
+		Coredata: ep,
+	})
+
+	testdataPath := test.FindTestdataFolder(t)
+	s, err := keyshareserver.New(&keyshareserver.Configuration{
+		SchemesPath:           filepath.Join(testdataPath, "irma_configuration"),
+		IssuerPrivateKeysPath: filepath.Join(testdataPath, "privatekeys"),
+		URL:                   "http://localhost:8080/irma_keyshare_server/",
+		DB:                    db,
+		JwtKeyId:              0,
+		JwtPrivateKeyFile:     filepath.Join(testdataPath, "jwtkeys", "kss-sk.pem"),
+		StoragePrimaryKeyFile: filepath.Join(testdataPath, "keyshareStorageTestkey"),
+		KeyshareCredential:    "test.test.mijnirma",
+		KeyshareAttribute:     "email",
+	})
+	require.NoError(t, err)
+
+	r := chi.NewRouter()
+	r.Mount("/irma_keyshare_server/", s.Handler())
+
+	serv = &http.Server{
+		Addr:    "localhost:8080",
+		Handler: r,
+	}
+
+	go func() {
+		err := serv.ListenAndServe()
+		if err == http.ErrServerClosed {
+			err = nil
+		}
+		assert.NoError(t, err)
+	}()
+}
+
+func stopKeyshareServer(t *testing.T) {
+	err := serv.Shutdown(context.Background())
+	assert.NoError(t, err)
+}
+
 func TestManualKeyshareSession(t *testing.T) {
+	startKeyshareServer(t)
+	defer stopKeyshareServer(t)
 	request := irma.NewSignatureRequest("I owe you everything", irma.NewAttributeTypeIdentifier("test.test.mijnirma.email"))
 	ms := createManualSessionHandler(t, nil)
 
@@ -22,10 +84,14 @@ func TestManualKeyshareSession(t *testing.T) {
 }
 
 func TestRequestorIssuanceKeyshareSession(t *testing.T) {
+	startKeyshareServer(t)
+	defer stopKeyshareServer(t)
 	testRequestorIssuance(t, true, nil)
 }
 
 func TestKeyshareRegister(t *testing.T) {
+	startKeyshareServer(t)
+	defer stopKeyshareServer(t)
 	client, handler := parseStorage(t)
 	defer test.ClearTestStorage(t, handler.storage)
 
@@ -49,6 +115,8 @@ func TestKeyshareRegister(t *testing.T) {
 // in a keyshare session of each session type.
 // Use keyshareuser.sql to enroll the user at the keyshare server.
 func TestKeyshareSessions(t *testing.T) {
+	startKeyshareServer(t)
+	defer stopKeyshareServer(t)
 	client, handler := parseStorage(t)
 	defer test.ClearTestStorage(t, handler.storage)
 	StartIrmaServer(t, false, "")
@@ -81,6 +149,8 @@ func keyshareSessions(t *testing.T, client *irmaclient.Client) {
 func TestIssuanceCombinedMultiSchemeSession(t *testing.T) {
 	StartIrmaServer(t, false, "")
 	defer StopIrmaServer()
+	startKeyshareServer(t)
+	defer stopKeyshareServer(t)
 
 	id := irma.NewAttributeTypeIdentifier("test.test.mijnirma.email")
 	request := getCombinedIssuanceRequest(id)
@@ -96,25 +166,4 @@ func TestIssuanceCombinedMultiSchemeSession(t *testing.T) {
 		},
 	}, id)
 	requestorSessionHelper(t, request, nil, sessionOptionReuseServer)
-}
-
-func TestKeyshareRevocation(t *testing.T) {
-	t.Run("Keyshare", func(t *testing.T) {
-		startRevocationServer(t, true)
-		defer stopRevocationServer()
-		client, handler := parseStorage(t)
-		defer test.ClearTestStorage(t, handler.storage)
-
-		testRevocation(t, revKeyshareTestAttr, client, handler)
-	})
-
-	t.Run("Both", func(t *testing.T) {
-		startRevocationServer(t, true)
-		defer stopRevocationServer()
-		client, handler := parseStorage(t)
-		defer test.ClearTestStorage(t, handler.storage)
-
-		testRevocation(t, revKeyshareTestAttr, client, handler)
-		testRevocation(t, revocationTestAttr, client, handler)
-	})
 }
