69caefc8e7d67a523d4092b381953316fd916ec8
David Venhoek <d.venhoek@privacybydesign.foundation>
2020-01-30 16:05:00 +0100

    Moved pinchange test to new keyshare server.

--- internal/sessiontest/keyshare_test.go
+++ internal/sessiontest/keyshare_test.go
@@ -1,78 +1,17 @@
 package sessiontest
 
 import (
-	"context"
-	"encoding/base64"
-	"net/http"
-	"path/filepath"
 	"testing"
-
-	"github.com/go-chi/chi"
-	"github.com/privacybydesign/irmago/internal/keysharecore"
 
 	irma "github.com/privacybydesign/irmago"
 	"github.com/privacybydesign/irmago/internal/test"
 	"github.com/privacybydesign/irmago/irmaclient"
-	"github.com/privacybydesign/irmago/server/keyshareserver"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-var serv *http.Server
-
-func startKeyshareServer(t *testing.T) {
-	db := keyshareserver.NewMemoryDatabase()
-	db.NewUser(keyshareserver.KeyshareUserData{
-		Username: "",
-		Coredata: keysharecore.EncryptedKeysharePacket{},
-	})
-	var ep keysharecore.EncryptedKeysharePacket
-	p, err := base64.StdEncoding.DecodeString("YWJjZB7irkDzwMWtBC6PTItWmO2AgAGm1/gFOyrd+nyt3/0GaHLY5Z1S1TM6N5nzb1Jh+Nqx0z0c3f9R2UyoYuy+pnrerTpYL1mpoZZfz8MPqcrAMsmVdb2kHH0BuAGSC0V28tp1BCVzhYnfMJyrUlNWonsTWSn68Av1BwpIBOGxqBXYfW0JzaffuSmZIyubImmTN7p32ASbseJSNwu0Rg==")
-	require.NoError(t, err)
-	copy(ep[:], p)
-	db.NewUser(keyshareserver.KeyshareUserData{
-		Username: "testusername",
-		Coredata: ep,
-	})
-
-	testdataPath := test.FindTestdataFolder(t)
-	s, err := keyshareserver.New(&keyshareserver.Configuration{
-		SchemesPath:           filepath.Join(testdataPath, "irma_configuration"),
-		URL:                   "http://localhost:8080/irma_keyshare_server/",
-		DB:                    db,
-		JwtKeyId:              0,
-		JwtPrivateKeyFile:     filepath.Join(testdataPath, "jwtkeys", "kss-sk.pem"),
-		StoragePrimaryKeyFile: filepath.Join(testdataPath, "keyshareStorageTestkey"),
-		KeyshareCredential:    "test.test.mijnirma",
-		KeyshareAttribute:     "email",
-	})
-	require.NoError(t, err)
-
-	r := chi.NewRouter()
-	r.Mount("/irma_keyshare_server/", s.Handler())
-
-	serv = &http.Server{
-		Addr:    "localhost:8080",
-		Handler: r,
-	}
-
-	go func() {
-		err := serv.ListenAndServe()
-		if err == http.ErrServerClosed {
-			err = nil
-		}
-		assert.NoError(t, err)
-	}()
-}
-
-func stopKeyshareServer(t *testing.T) {
-	err := serv.Shutdown(context.Background())
-	assert.NoError(t, err)
-}
-
 func TestManualKeyshareSession(t *testing.T) {
-	startKeyshareServer(t)
-	defer stopKeyshareServer(t)
+	test.StartKeyshareServer(t)
+	defer test.StopKeyshareServer(t)
 	request := irma.NewSignatureRequest("I owe you everything", irma.NewAttributeTypeIdentifier("test.test.mijnirma.email"))
 	ms := createManualSessionHandler(t, nil)
 
@@ -83,14 +22,14 @@ func TestManualKeyshareSession(t *testing.T) {
 }
 
 func TestRequestorIssuanceKeyshareSession(t *testing.T) {
-	startKeyshareServer(t)
-	defer stopKeyshareServer(t)
+	test.startKeyshareServer(t)
+	defer test.stopKeyshareServer(t)
 	testRequestorIssuance(t, true, nil)
 }
 
 func TestKeyshareRegister(t *testing.T) {
-	startKeyshareServer(t)
-	defer stopKeyshareServer(t)
+	test.StartKeyshareServer(t)
+	defer test.StopKeyshareServer(t)
 	client, handler := parseStorage(t)
 	defer test.ClearTestStorage(t, handler.storage)
 
@@ -111,8 +50,8 @@ func TestKeyshareRegister(t *testing.T) {
 // in a keyshare session of each session type.
 // Use keyshareuser.sql to enroll the user at the keyshare server.
 func TestKeyshareSessions(t *testing.T) {
-	startKeyshareServer(t)
-	defer stopKeyshareServer(t)
+	test.startKeyshareServer(t)
+	defer test.stopKeyshareServer(t)
 	client, handler := parseStorage(t)
 	defer test.ClearTestStorage(t, handler.storage)
 	keyshareSessions(t, client)
@@ -141,8 +80,8 @@ func keyshareSessions(t *testing.T, client *irmaclient.Client) {
 }
 
 func TestIssuanceCombinedMultiSchemeSession(t *testing.T) {
-	startKeyshareServer(t)
-	defer stopKeyshareServer(t)
+	test.StartKeyshareServer(t)
+	defer test.StopKeyshareServer(t)
 	id := irma.NewAttributeTypeIdentifier("test.test.mijnirma.email")
 	request := getCombinedIssuanceRequest(id)
 	sessionHelper(t, request, "issue", nil)
--- irmaclient/irmaclient_keyshare_test.go
+++ irmaclient/irmaclient_keyshare_test.go
@@ -1,19 +1,19 @@
-// +build !local_tests
-
 package irmaclient
 
 import (
 	"testing"
 
-	"github.com/privacybydesign/irmago"
+	irma "github.com/privacybydesign/irmago"
 	"github.com/privacybydesign/irmago/internal/test"
 	"github.com/stretchr/testify/require"
 )
 
 // Test pinchange interaction
 func TestKeyshareChangePin(t *testing.T) {
+	test.StartKeyshareServer(t)
+	defer test.StopKeyshareServer(t)
 	client, handler := parseStorage(t)
-	defer test.ClearTestStorage(t, handler.storage)
+	defer test.ClearTestStorage(t handler.storage)
 
 	require.NoError(t, client.keyshareChangePinWorker(irma.NewSchemeManagerIdentifier("test"), "12345", "54321"))
 	require.NoError(t, client.keyshareChangePinWorker(irma.NewSchemeManagerIdentifier("test"), "54321", "12345"))
