b0ce9a706e71c127f47984bf2428f14f1dbc85a0
Sietse Ringers <mail@sietseringers.net>
2020-03-06 14:52:50 +0100

    chore: switch from dep to Go modules
    Closes #78 and #84.

--- version.go
+++ version.go
@@ -4,16 +4,5 @@
 // contains attribute and credential verification logic.
 package irma
 
-import "github.com/timshannon/bolthold"
-
 // Version of the IRMA command line and libraries
 const Version = "0.5.0-rc.1"
-
-// go-atum requires a version of bolthold newer than the latest release v1.1, but go-atum does not
-// use dep, so by default dep fetches v1.1 which breaks the build. We make bolthold an explicit
-// dependency here, so that we can require its version to be sufficiently new in a [[constraint]] in
-// Gopkg.toml: an [[override]] would not propagate to the users of irmago. This is ok, as we will
-// have bolthold as actual dependency soon anyway. go-atum uses go mod files which does properly
-// lock its bolthold dependency, so:
-// TODO: remove this line after we switch to go modules
-var _ = bolthold.Key
