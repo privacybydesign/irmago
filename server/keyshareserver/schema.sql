ccf9748be982c1b243bc76529957bd634e8d79e9
David Venhoek <d.venhoek@privacybydesign.foundation>
2020-03-17 12:45:58 +0100

    Added database support to myirmaserver.

--- server/keyshareserver/schema.sql
+++ server/keyshareserver/schema.sql
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS irma.log_entry_records
     time bigint NOT NULL,
     event text NOT NULL,
     param text,
-    user_id int NOT NULL
+    user_id int NOT NULL REFERENCES irma.users (id) ON DELETE CASCADE
 );
 CREATE INDEX log_entry_records_user_id_index ON irma.log_entry_records (user_id, time);
 
@@ -27,15 +27,24 @@ CREATE TABLE IF NOT EXISTS irma.email_verification_tokens
     id serial PRIMARY KEY,
     token text NOT NULL,
     email text NOT NULL,
-    user_id int NOT NULL
+    user_id int NOT NULL REFERENCES irma.users (id) ON DELETE CASCADE
 );
 CREATE UNIQUE INDEX email_verification_token_index ON irma.email_verification_tokens (token);
+
+CREATE TABLE IF NOT EXISTS irma.email_login_tokens
+(
+    id serial PRIMARY KEY,
+    token text NOT NULL,
+    email text NOT NULL
+);
+CREATE UNIQUE INDEX email_login_token_index ON irma.email_login_tokens (token);
 
 CREATE TABLE IF NOT EXISTS irma.email_addresses
 (
     id serial PRIMARY KEY,
-    user_id int NOT NULL,
+    user_id int NOT NULL REFERENCES irma.users (id) ON DELETE CASCADE,
     emailAddress text NOT NULL
 );
 CREATE INDEX emailAddress_index ON irma.email_addresses (emailAddress);
 CREATE INDEX emailAddress_userid_index ON irma.email_addresses (user_id);
+CREATE UNIQUE INDEX emailAddress_constraint_index ON irma.email_addresses (user_id, emailAddress);
