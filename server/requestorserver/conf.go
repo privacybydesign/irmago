bbf15f2ff0f93f1397c5f66381a7d55f709f279f
Sietse Ringers <mail@sietseringers.net>
2020-03-30 17:29:19 +0200

    feat: update supported TLS ciphers and curves
    - Remove NIST P curves as they are not safe according to https://safecurves.cr.yp.to/.
      Only X25519 is now supported but according to https://en.wikipedia.org/wiki/Comparison_of_TLS_implementations#Supported_elliptic_curves
      all relevant implementations support this curve.
    - Remove CBC ciphers, add ChaCha20-Poly1305 ciphers, add other GCM ciphers

--- server/requestorserver/conf.go
+++ server/requestorserver/conf.go
@@ -387,17 +387,23 @@ func (conf *Configuration) readTlsConf(cert, certfile, key, keyfile string) (*tl
 		return nil, err
 	}
 	return &tls.Config{
-		Certificates:             []tls.Certificate{cer},
-		MinVersion:               tls.VersionTLS12,
-		CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+		Certificates: []tls.Certificate{cer},
+		MinVersion:   tls.VersionTLS12,
+
+		// Safe according to https://safecurves.cr.yp.to/; fairly widely supported according to
+		// https://en.wikipedia.org/wiki/Comparison_of_TLS_implementations#Supported_elliptic_curves
+		CurvePreferences: []tls.CurveID{tls.X25519},
+
 		PreferServerCipherSuites: true,
 		CipherSuites: []uint16{
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
 			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
 			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
 			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
 			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
 		},
 	}, nil
 }
