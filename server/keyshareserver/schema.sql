64c4a4e5d89ea6b40f4950cff030cecd6574010c
David Venhoek <d.venhoek@privacybydesign.foundation>
2020-05-13 08:48:28 +0200

    Refactored database layout to be more consistent.

--- server/keyshareserver/schema.sql
+++ server/keyshareserver/schema.sql
@@ -6,9 +6,9 @@ CREATE TABLE IF NOT EXISTS irma.users
     username text NOT NULL,
     language text NOT NULL,
     coredata bytea,
-    lastSeen bigint NOT NULL,
-    pinCounter int NOT NULL,
-    pinBlockDate bigint NOT NULL,
+    last_seen bigint NOT NULL,
+    pin_counter int NOT NULL,
+    pin_block_date bigint NOT NULL,
     delete_on bigint
 );
 CREATE UNIQUE INDEX username_index ON irma.users (username);
@@ -42,13 +42,13 @@ CREATE TABLE IF NOT EXISTS irma.email_login_tokens
 );
 CREATE UNIQUE INDEX email_login_token_index ON irma.email_login_tokens (token);
 
-CREATE TABLE IF NOT EXISTS irma.email_addresses
+CREATE TABLE IF NOT EXISTS irma.emails
 (
     id serial PRIMARY KEY,
     user_id int NOT NULL REFERENCES irma.users (id) ON DELETE CASCADE,
-    emailAddress text NOT NULL,
+    email text NOT NULL,
     delete_on bigint
 );
-CREATE INDEX emailAddress_index ON irma.email_addresses (emailAddress);
-CREATE INDEX emailAddress_userid_index ON irma.email_addresses (user_id);
-CREATE UNIQUE INDEX emailAddress_constraint_index ON irma.email_addresses (user_id, emailAddress);
+CREATE INDEX email_index ON irma.emails (email);
+CREATE INDEX email_userid_index ON irma.emails (user_id);
+CREATE UNIQUE INDEX email_constraint_index ON irma.emails (user_id, email);
