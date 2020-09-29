c49b79496b1994971df7eccfbd8550cb26ddd173
Leon <leon.botros@ru.nl>
2020-09-29 15:49:07 +0200

    fix: avoid possible panic by checking for SessionError type

--- irmaclient/session.go
+++ irmaclient/session.go
@@ -326,7 +326,11 @@ func (session *session) processSessionInfo() {
 		issuedAt := time.Now()
 		_, err := ir.GetCredentialInfoList(session.client.Configuration, session.Version, issuedAt)
 		if err != nil {
-			session.fail(err.(*irma.SessionError))
+			if err, ok := err.(*irma.SessionError); ok {
+				session.fail(err)
+			} else {
+				session.fail(&irma.SessionError{ErrorType: irma.ErrorUnknownIdentifier, Err: err})
+			}
 			return
 		}
 
