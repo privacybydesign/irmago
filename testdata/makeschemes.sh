f44cee4d1c2d92bcfac4bfad4bdc7ff328f54406
Sietse Ringers <mail@sietseringers.net>
2020-11-25 18:10:35 +0100

    feat: add test-requestors scheme to updated schemes in testdata

--- testdata/makeschemes.sh
+++ testdata/makeschemes.sh
@@ -18,6 +18,10 @@ sleep 1
 git checkout -- ${dir}/irma_configuration_updated/irma-demo/RU/Issues/studentCard/description.xml
 irma scheme sign ${dir}/irma_configuration/irma-demo/sk.pem ${dir}/irma_configuration_updated/irma-demo
 
+# restore changes to requestor scheme, then resign
+git checkout -- ${dir}/irma_configuration_updated/test-requestors/requestors.json
+irma scheme sign ${dir}/irma_configuration/test-requestors/sk.pem ${dir}/irma_configuration_updated/test-requestors
+
 # resign, then restore changes to studentCard credtype, invalidating the scheme
 irma scheme sign ${dir}/irma_configuration/irma-demo/sk.pem ${dir}/irma_configuration_invalid/irma-demo
 git checkout -- ${dir}/irma_configuration_invalid/irma-demo/RU/Issues/studentCard/description.xml
