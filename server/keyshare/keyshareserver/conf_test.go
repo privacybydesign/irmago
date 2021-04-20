0429f6ad6f05ab19d0fada2dab58437c85a55f36
Sietse Ringers <mail@sietseringers.net>
2021-04-20 14:14:51 +0200

    test: fix code duplication in keyshare server configuration test

--- server/keyshare/keyshareserver/conf_test.go
+++ server/keyshare/keyshareserver/conf_test.go
@@ -12,258 +12,139 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func validConf(t *testing.T) *Configuration {
+	testdataPath := test.FindTestdataFolder(t)
+	return &Configuration{
+		Configuration: &server.Configuration{
+			SchemesPath: filepath.Join(testdataPath, "irma_configuration"),
+			Logger:      irma.Logger,
+		},
+		KeyshareURL:           "http://localhost:8080/irma_keyshare_server/",
+		DBType:                DatabaseTypeMemory,
+		JwtKeyID:              0,
+		JwtPrivateKeyFile:     filepath.Join(testdataPath, "jwtkeys", "kss-sk.pem"),
+		StoragePrimaryKeyFile: filepath.Join(testdataPath, "keyshareStorageTestkey"),
+		KeyshareCredential:    "test.test.mijnirma",
+		KeyshareAttribute:     "email",
+	}
+}
+
+func validConfWithEmail(t *testing.T) *Configuration {
+	conf := validConf(t)
+	conf.EmailServer = "doesnotexist"
+	conf.DefaultLanguage = "en"
+	return conf
+}
+
 func TestConfInvalidAESKey(t *testing.T) {
 	testdataPath := test.FindTestdataFolder(t)
 
-	_, err := New(&Configuration{
-		Configuration: &server.Configuration{
-			SchemesPath: filepath.Join(testdataPath, "irma_configuration"),
-			Logger:      irma.Logger,
-		},
-		KeyshareURL:           "http://localhost:8080/irma_keyshare_server/",
-		DBType:                DatabaseTypeMemory,
-		JwtKeyID:              0,
-		JwtPrivateKeyFile:     filepath.Join(testdataPath, "jwtkeys", "kss-sk.pem"),
-		StoragePrimaryKeyFile: filepath.Join(testdataPath, "keyshareStorageTestkey"),
-		KeyshareCredential:    "test.test.mijnirma",
-		KeyshareAttribute:     "email",
-	})
+	_, err := New(validConf(t))
 	assert.NoError(t, err)
 
-	_, err = New(&Configuration{
-		Configuration: &server.Configuration{
-			SchemesPath: filepath.Join(testdataPath, "irma_configuration"),
-			Logger:      irma.Logger,
-		},
-		KeyshareURL:           "http://localhost:8080/irma_keyshare_server/",
-		DBType:                DatabaseTypeMemory,
-		JwtKeyID:              0,
-		JwtPrivateKeyFile:     filepath.Join(testdataPath, "jwtkeys", "kss-sk-does-not-exist.pem"),
-		StoragePrimaryKeyFile: filepath.Join(testdataPath, "keyshareStorageTestkey"),
-		KeyshareCredential:    "test.test.mijnirma",
-		KeyshareAttribute:     "email",
-	})
+	conf := validConf(t)
+	conf.JwtPrivateKeyFile = filepath.Join(testdataPath, "jwtkeys", "kss-sk-does-not-exist.pem")
+	_, err = New(conf)
 	assert.Error(t, err)
 
-	_, err = New(&Configuration{
-		Configuration: &server.Configuration{
-			SchemesPath: filepath.Join(testdataPath, "irma_configuration"),
-			Logger:      irma.Logger,
-		},
-		KeyshareURL:           "http://localhost:8080/irma_keyshare_server/",
-		DBType:                DatabaseTypeMemory,
-		JwtKeyID:              0,
-		JwtPrivateKeyFile:     filepath.Join(testdataPath, "jwtkeys", "kss-sk.pem"),
-		StoragePrimaryKeyFile: filepath.Join(testdataPath, "keyshareStorageTestkey-does-not-exist"),
-		KeyshareCredential:    "test.test.mijnirma",
-		KeyshareAttribute:     "email",
-	})
+	conf = validConf(t)
+	conf.StoragePrimaryKeyFile = filepath.Join(testdataPath, "keyshareStorageTestkey-does-not-exist")
+	_, err = New(conf)
 	assert.Error(t, err)
 
-	_, err = New(&Configuration{
-		Configuration: &server.Configuration{
-			SchemesPath: filepath.Join(testdataPath, "irma_configuration"),
-			Logger:      irma.Logger,
-		},
-		KeyshareURL:           "http://localhost:8080/irma_keyshare_server/",
-		DBType:                DatabaseTypeMemory,
-		JwtKeyID:              0,
-		JwtPrivateKeyFile:     filepath.Join(testdataPath, "jwtkeys", "kss-sk.pem"),
-		StoragePrimaryKeyFile: filepath.Join(testdataPath, "jwtkeys", "kss-sk.pem"),
-		KeyshareCredential:    "test.test.mijnirma",
-		KeyshareAttribute:     "email",
-	})
+	conf = validConf(t)
+	conf.StoragePrimaryKeyFile = filepath.Join(testdataPath, "jwtkeys", "kss-sk.pem")
+	_, err = New(conf)
 	assert.Error(t, err)
 
-	_, err = New(&Configuration{
-		Configuration: &server.Configuration{
-			SchemesPath: filepath.Join(testdataPath, "irma_configuration"),
-			Logger:      irma.Logger,
-		},
-		KeyshareURL:           "http://localhost:8080/irma_keyshare_server/",
-		DBType:                "undefined",
-		JwtKeyID:              0,
-		JwtPrivateKeyFile:     filepath.Join(testdataPath, "jwtkeys", "kss-sk.pem"),
-		StoragePrimaryKeyFile: filepath.Join(testdataPath, "keyshareStorageTestkey"),
-		KeyshareCredential:    "test.test.mijnirma",
-		KeyshareAttribute:     "email",
-	})
+	conf = validConf(t)
+	conf.DBType = "undefined"
+	_, err = New(conf)
 	assert.Error(t, err)
 
-	_, err = New(&Configuration{
-		Configuration: &server.Configuration{
-			SchemesPath: filepath.Join(testdataPath, "irma_configuration"),
-			Logger:      irma.Logger,
-		},
-		KeyshareURL:           "http://localhost:8080/irma_keyshare_server/",
-		DBType:                DatabaseTypeMemory,
-		JwtKeyID:              0,
-		JwtPrivateKeyFile:     filepath.Join(testdataPath, "jwtkeys", "kss-sk.pem"),
-		StoragePrimaryKeyFile: filepath.Join(testdataPath, "keyshareStorageTestkey"),
-		KeyshareCredential:    "test.test.mijnirma",
-		KeyshareAttribute:     "email",
-		EmailServer:           "doesnotexist",
-		DefaultLanguage:       "en",
-		RegistrationEmailSubject: map[string]string{
-			"en": "testsubject",
-		},
-		VerificationURL: map[string]string{
-			"en": "test",
-		},
-	})
+	conf = validConfWithEmail(t)
+	conf.RegistrationEmailSubject = map[string]string{
+		"en": "testsubject",
+	}
+	conf.VerificationURL = map[string]string{
+		"en": "test",
+	}
+	_, err = New(conf)
 	assert.Error(t, err)
 
 	testTemplate := template.New("test")
 	_, err = testTemplate.Parse("testtemplate {{.VerificationURL}}")
 	require.NoError(t, err)
 
-	_, err = New(&Configuration{
-		Configuration: &server.Configuration{
-			SchemesPath: filepath.Join(testdataPath, "irma_configuration"),
-			Logger:      irma.Logger,
-		},
-		KeyshareURL:           "http://localhost:8080/irma_keyshare_server/",
-		DBType:                DatabaseTypeMemory,
-		JwtKeyID:              0,
-		JwtPrivateKeyFile:     filepath.Join(testdataPath, "jwtkeys", "kss-sk.pem"),
-		StoragePrimaryKeyFile: filepath.Join(testdataPath, "keyshareStorageTestkey"),
-		KeyshareCredential:    "test.test.mijnirma",
-		KeyshareAttribute:     "email",
-		EmailServer:           "doesnotexist",
-		DefaultLanguage:       "en",
-		RegistrationEmailTemplates: map[string]*template.Template{
-			"en": testTemplate,
-		},
-		VerificationURL: map[string]string{
-			"en": "test",
-		},
-	})
+	conf = validConfWithEmail(t)
+	conf.RegistrationEmailTemplates = map[string]*template.Template{
+		"en": testTemplate,
+	}
+	conf.VerificationURL = map[string]string{
+		"en": "test",
+	}
+	_, err = New(conf)
 	assert.Error(t, err)
 
-	_, err = New(&Configuration{
-		Configuration: &server.Configuration{
-			SchemesPath: filepath.Join(testdataPath, "irma_configuration"),
-			Logger:      irma.Logger,
-		},
-		KeyshareURL:           "http://localhost:8080/irma_keyshare_server/",
-		DBType:                DatabaseTypeMemory,
-		JwtKeyID:              0,
-		JwtPrivateKeyFile:     filepath.Join(testdataPath, "jwtkeys", "kss-sk.pem"),
-		StoragePrimaryKeyFile: filepath.Join(testdataPath, "keyshareStorageTestkey"),
-		KeyshareCredential:    "test.test.mijnirma",
-		KeyshareAttribute:     "email",
-		EmailServer:           "doesnotexist",
-		DefaultLanguage:       "en",
-		RegistrationEmailTemplates: map[string]*template.Template{
-			"en": testTemplate,
-		},
-		RegistrationEmailSubject: map[string]string{
-			"en": "testsubject",
-		},
-	})
+	conf = validConfWithEmail(t)
+	conf.RegistrationEmailTemplates = map[string]*template.Template{
+		"en": testTemplate,
+	}
+	conf.RegistrationEmailSubject = map[string]string{
+		"en": "testsubject",
+	}
+	_, err = New(conf)
 	assert.Error(t, err)
 
-	_, err = New(&Configuration{
-		Configuration: &server.Configuration{
-			SchemesPath: filepath.Join(testdataPath, "irma_configuration"),
-			Logger:      irma.Logger,
-		},
-		KeyshareURL:           "http://localhost:8080/irma_keyshare_server/",
-		DBType:                DatabaseTypeMemory,
-		JwtKeyID:              0,
-		JwtPrivateKeyFile:     filepath.Join(testdataPath, "jwtkeys", "kss-sk.pem"),
-		StoragePrimaryKeyFile: filepath.Join(testdataPath, "keyshareStorageTestkey"),
-		KeyshareCredential:    "test.test.mijnirma",
-		KeyshareAttribute:     "email",
-		EmailServer:           "doesnotexist",
-		DefaultLanguage:       "en",
-		RegistrationEmailTemplates: map[string]*template.Template{
-			"en": testTemplate,
-		},
-		RegistrationEmailSubject: map[string]string{
-			"en": "testsubject",
-		},
-		VerificationURL: map[string]string{
-			"en": "test",
-		},
-	})
+	conf = validConfWithEmail(t)
+	conf.RegistrationEmailTemplates = map[string]*template.Template{
+		"en": testTemplate,
+	}
+	conf.RegistrationEmailSubject = map[string]string{
+		"en": "testsubject",
+	}
+	conf.VerificationURL = map[string]string{
+		"en": "test",
+	}
+	_, err = New(conf)
 	assert.NoError(t, err)
 
-	_, err = New(&Configuration{
-		Configuration: &server.Configuration{
-			SchemesPath: filepath.Join(testdataPath, "irma_configuration"),
-			Logger:      irma.Logger,
-		},
-		KeyshareURL:           "http://localhost:8080/irma_keyshare_server/",
-		DBType:                DatabaseTypeMemory,
-		JwtKeyID:              0,
-		JwtPrivateKeyFile:     filepath.Join(testdataPath, "jwtkeys", "kss-sk.pem"),
-		StoragePrimaryKeyFile: filepath.Join(testdataPath, "keyshareStorageTestkey"),
-		KeyshareCredential:    "test.test.mijnirma",
-		KeyshareAttribute:     "email",
-		EmailServer:           "doesnotexist",
-		DefaultLanguage:       "en",
-		RegistrationEmailFiles: map[string]string{
-			"en": filepath.Join(testdataPath, "does-not-exist"),
-		},
-		RegistrationEmailSubject: map[string]string{
-			"en": "testsubject",
-		},
-		VerificationURL: map[string]string{
-			"en": "test",
-		},
-	})
+	conf = validConfWithEmail(t)
+	conf.RegistrationEmailFiles = map[string]string{
+		"en": filepath.Join(testdataPath, "does-not-exist"),
+	}
+	conf.RegistrationEmailSubject = map[string]string{
+		"en": "testsubject",
+	}
+	conf.VerificationURL = map[string]string{
+		"en": "test",
+	}
+	_, err = New(conf)
 	assert.Error(t, err)
 
-	_, err = New(&Configuration{
-		Configuration: &server.Configuration{
-			SchemesPath: filepath.Join(testdataPath, "irma_configuration"),
-			Logger:      irma.Logger,
-		},
-		KeyshareURL:           "http://localhost:8080/irma_keyshare_server/",
-		DBType:                DatabaseTypeMemory,
-		JwtKeyID:              0,
-		JwtPrivateKeyFile:     filepath.Join(testdataPath, "jwtkeys", "kss-sk.pem"),
-		StoragePrimaryKeyFile: filepath.Join(testdataPath, "keyshareStorageTestkey"),
-		KeyshareCredential:    "test.test.mijnirma",
-		KeyshareAttribute:     "email",
-		EmailServer:           "doesnotexist",
-		DefaultLanguage:       "en",
-		RegistrationEmailFiles: map[string]string{
-			"en": filepath.Join(testdataPath, "emailtemplate.html"),
-		},
-		RegistrationEmailSubject: map[string]string{
-			"en": "testsubject",
-		},
-		VerificationURL: map[string]string{
-			"en": "test",
-		},
-	})
+	conf = validConfWithEmail(t)
+	conf.RegistrationEmailFiles = map[string]string{
+		"en": filepath.Join(testdataPath, "emailtemplate.html"),
+	}
+	conf.RegistrationEmailSubject = map[string]string{
+		"en": "testsubject",
+	}
+	conf.VerificationURL = map[string]string{
+		"en": "test",
+	}
+	_, err = New(conf)
 	assert.NoError(t, err)
 
-	_, err = New(&Configuration{
-		Configuration: &server.Configuration{
-			SchemesPath: filepath.Join(testdataPath, "irma_configuration"),
-			Logger:      irma.Logger,
-		},
-		KeyshareURL:           "http://localhost:8080/irma_keyshare_server/",
-		DBType:                DatabaseTypeMemory,
-		JwtKeyID:              0,
-		JwtPrivateKeyFile:     filepath.Join(testdataPath, "jwtkeys", "kss-sk.pem"),
-		StoragePrimaryKeyFile: filepath.Join(testdataPath, "keyshareStorageTestkey"),
-		KeyshareCredential:    "test.test.mijnirma",
-		KeyshareAttribute:     "email",
-		EmailServer:           "doesnotexist",
-		DefaultLanguage:       "en",
-		RegistrationEmailFiles: map[string]string{
-			"en": filepath.Join(testdataPath, "invalidemailtemplate.html"),
-		},
-		RegistrationEmailSubject: map[string]string{
-			"en": "testsubject",
-		},
-		VerificationURL: map[string]string{
-			"en": "test",
-		},
-	})
+	conf = validConfWithEmail(t)
+	conf.RegistrationEmailFiles = map[string]string{
+		"en": filepath.Join(testdataPath, "invalidemailtemplate.html"),
+	}
+	conf.RegistrationEmailSubject = map[string]string{
+		"en": "testsubject",
+	}
+	conf.VerificationURL = map[string]string{
+		"en": "test",
+	}
+	_, err = New(conf)
 	assert.Error(t, err)
 }
