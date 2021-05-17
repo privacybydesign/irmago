cc61b43ae6311c5995d4d92fcaf010a0e105814a
Sietse Ringers <mail@sietseringers.net>
2021-05-17 12:02:23 +0200

    feat: improve error messages of irma keyshare keygen

--- irma/cmd/keyshare-keygen.go
+++ irma/cmd/keyshare-keygen.go
@@ -11,27 +11,30 @@ import (
 var keyshareKeygen = &cobra.Command{
 	Use:   "keygen",
 	Short: "Generate storage key for keyshare server",
-	RunE: func(command *cobra.Command, args []string) error {
+	Run: func(command *cobra.Command, args []string) {
 		filename, err := command.Flags().GetString("key-file")
 		if err != nil {
-			return err
+			die("", err)
 		}
 
 		counter, err := command.Flags().GetUint32("counter")
 		if err != nil {
-			return err
+			die("", err)
 		}
 
 		key, err := keysharecore.GenerateAESKey()
 		if err != nil {
-			return err
+			die("", err)
 		}
 
 		keydata := make([]byte, 4+len(key[:]))
 		binary.LittleEndian.PutUint32(keydata, counter)
 		copy(keydata[4:], key[:])
 
-		return ioutil.WriteFile(filename, keydata, 0600)
+		err = ioutil.WriteFile(filename, keydata, 0600)
+		if err != nil {
+			die("", err)
+		}
 	},
 }
 
