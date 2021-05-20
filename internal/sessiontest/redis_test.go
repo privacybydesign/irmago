1d95b59ae7ccb887130c0d5201cd92cfabe78dd7
Maja Reissner <maja.reissner@sidn.nl>
2021-05-20 13:37:08 +0200

    Only run selected session tests when testing redis.

--- internal/sessiontest/redis_test.go
+++ internal/sessiontest/redis_test.go
@@ -84,7 +84,6 @@ func startLoadBalancer(t *testing.T, irmaServers []int) *http.Server {
 }
 
 func TestRedis(t *testing.T) {
-	// TODO: Consider whether it is necessary to execute all tests against Redis storage
 	defaultIrmaServerConfiguration := IrmaServerConfiguration
 	defaultJwtServerConfiguration := JwtServerConfiguration
 
@@ -97,35 +96,10 @@ func TestRedis(t *testing.T) {
 		JwtServerConfiguration = defaultJwtServerConfiguration
 	}()
 
-	// run all session_test tests
 	t.Run("TestSigningSession", TestSigningSession)
 	t.Run("TestDisclosureSession", TestDisclosureSession)
-	t.Run("TestNoAttributeDisclosureSession", TestNoAttributeDisclosureSession)
-	t.Run("TestEmptyDisclosure", TestEmptyDisclosure)
 	t.Run("TestIssuanceSession", TestIssuanceSession)
-	t.Run("TestMultipleIssuanceSession", TestMultipleIssuanceSession)
-	t.Run("TestDefaultCredentialValidity", TestDefaultCredentialValidity)
-	t.Run("TestIssuanceDisclosureEmptyAttributes", TestIssuanceDisclosureEmptyAttributes)
-	t.Run("TestIssuanceOptionalZeroLengthAttributes", TestIssuanceOptionalZeroLengthAttributes)
-	t.Run("TestIssuanceOptionalSetAttributes", TestIssuanceOptionalSetAttributes)
-	t.Run("TestIssuanceSameAttributesNotSingleton", TestIssuanceSameAttributesNotSingleton)
-	t.Run("TestLargeAttribute", TestLargeAttribute)
-	t.Run("TestIssuanceSingletonCredential", TestIssuanceSingletonCredential)
-	t.Run("TestUnsatisfiableDisclosureSession", TestUnsatisfiableDisclosureSession)
-	t.Run("TestAttributeByteEncoding", TestAttributeByteEncoding)
-	t.Run("TestOutdatedClientIrmaConfiguration", TestOutdatedClientIrmaConfiguration)
-	t.Run("TestDisclosureNewAttributeUpdateSchemeManager", TestDisclosureNewAttributeUpdateSchemeManager)
-	t.Run("TestIssueNewAttributeUpdateSchemeManager", TestIssueNewAttributeUpdateSchemeManager)
-	t.Run("TestIrmaServerPrivateKeysFolder", TestIrmaServerPrivateKeysFolder)
-	t.Run("TestIssueOptionalAttributeUpdateSchemeManager", TestIssueOptionalAttributeUpdateSchemeManager)
-	t.Run("TestIssueNewCredTypeUpdateSchemeManager", TestIssueNewCredTypeUpdateSchemeManager)
-	t.Run("TestDisclosureNewCredTypeUpdateSchemeManager", TestDisclosureNewCredTypeUpdateSchemeManager)
-	t.Run("TestDisclosureNonexistingCredTypeUpdateSchemeManager", TestDisclosureNonexistingCredTypeUpdateSchemeManager)
-	t.Run("TestStaticQRSession", TestStaticQRSession)
 	t.Run("TestIssuedCredentialIsStored", TestIssuedCredentialIsStored)
-	t.Run("TestBlindIssuanceSession", TestBlindIssuanceSession)
-	t.Run("TestBlindIssuanceSessionDifferentAmountOfRandomBlinds", TestBlindIssuanceSessionDifferentAmountOfRandomBlinds)
-	t.Run("TestPOSTSizeLimit", TestPOSTSizeLimit)
 	t.Run("TestChainedSessions", TestChainedSessions)
 	t.Run("TestUnknownRequestorToken", TestUnknownRequestorToken)
 }
