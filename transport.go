45201ae87f165ccfee7b7f733e6560c84cfd1b8b
Ivar Derksen <ivar.derksen@sidn.nl>
2021-05-28 10:18:10 +0200

    Added support for HTTP status code 204 when doing JSON requests

--- transport.go
+++ transport.go
@@ -242,7 +242,15 @@ func (transport *HTTPTransport) jsonRequest(url string, method string, result in
 	if err != nil {
 		return &SessionError{ErrorType: ErrorServerResponse, Err: err, RemoteStatus: res.StatusCode}
 	}
-	if res.StatusCode != 200 {
+	if res.StatusCode == http.StatusNoContent {
+		if result != nil {
+			return &SessionError{
+				ErrorType:    ErrorServerResponse,
+				Err:          errors.New("'204 No Content' received, but result was expected"),
+				RemoteStatus: res.StatusCode,
+			}
+		}
+	} else if res.StatusCode != http.StatusOK {
 		apierr := &RemoteError{}
 		err = transport.unmarshal(body, apierr)
 		if err != nil || apierr.ErrorName == "" { // Not an ApiErrorMessage
