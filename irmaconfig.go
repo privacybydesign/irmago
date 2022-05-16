952f8a8c3d00791bb68db79f59ba7e8106bd807c
Ivar Derksen <ivar.derksen@sidn.nl>
2022-05-17 01:37:48 +0200

    Revert: introduction of function newConfiguration that returns by value instead of a pointer

--- irmaconfig.go
+++ irmaconfig.go
@@ -90,16 +90,8 @@ type ConfigurationOptions struct {
 
 // NewConfiguration returns a new configuration. After this
 // ParseFolder() should be called to parse the specified path.
-func NewConfiguration(path string, opts ConfigurationOptions) (*Configuration, error) {
-	conf, err := newConfiguration(path, opts)
-	if err != nil {
-		return nil, err
-	}
-	return &conf, nil
-}
-
-func newConfiguration(path string, opts ConfigurationOptions) (conf Configuration, err error) {
-	conf = Configuration{
+func NewConfiguration(path string, opts ConfigurationOptions) (conf *Configuration, err error) {
+	conf = &Configuration{
 		Path:     path,
 		assets:   opts.Assets,
 		readOnly: opts.ReadOnly,
@@ -108,11 +100,11 @@ func newConfiguration(path string, opts ConfigurationOptions) (conf Configuratio
 
 	if conf.assets != "" { // If an assets folder is specified, then it must exist
 		if err = common.AssertPathExists(conf.assets); err != nil {
-			return conf, errors.WrapPrefix(err, "Nonexistent assets folder specified", 0)
+			return nil, errors.WrapPrefix(err, "Nonexistent assets folder specified", 0)
 		}
 	}
 	if err = common.EnsureDirectoryExists(conf.Path); err != nil {
-		return conf, err
+		return nil, err
 	}
 
 	// Init all maps
