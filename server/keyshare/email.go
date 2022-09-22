36792776826bd2c2e2b0c3c6bfa31bad66a0d51e
Sander Hollaar <sander@humilis.net>
2022-09-22 10:50:33 +0200

    review feedback: implement checks on variables before usage

--- server/keyshare/email.go
+++ server/keyshare/email.go
@@ -138,9 +138,15 @@ func sendHTMLEmail(addr string, a smtp.Auth, from, to *mail.Address, subject str
 
 func verifyMXRecord(email string) error {
 	at := strings.LastIndex(email, "@")
-	_, err := net.LookupMX(email[at+1:])
-	if err != nil {
-		return err
+	if at > 0 {
+		records, err := net.LookupMX(email[at+1:])
+		if len(records) == 0 {
+			return errors.Errorf("No domain part found in %v", email)
+		} else if err != nil {
+			return err
+		}
+	} else {
+		return errors.Errorf("No '@'-sign found in %v", email)
 	}
 	return nil
 }
