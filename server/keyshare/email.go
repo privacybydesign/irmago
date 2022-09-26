dfcf6110b4f3a6cae64f26c00b768a75a67df8df
Sander Hollaar <sander@humilis.net>
2022-09-26 10:19:14 +0200

    lowercase ErrInvalidEmail errormessage

--- server/keyshare/email.go
+++ server/keyshare/email.go
@@ -19,7 +19,7 @@ type EmailConfiguration struct {
 	EmailAuth       smtp.Auth
 }
 
-var ErrInvalidEmail = errors.New("Invalid email address")
+var ErrInvalidEmail = errors.New("invalid email address")
 
 func ParseEmailTemplates(files, subjects map[string]string, defaultLanguage string) (map[string]*template.Template, error) {
 	if _, ok := files[defaultLanguage]; !ok {
