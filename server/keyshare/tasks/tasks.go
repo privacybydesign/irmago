c25526112539542a3f7e630518994f76fccdc86d
Bob Hageman <bob@schluss.org>
2023-06-02 12:04:37 +0200

    Implement email revalidation

--- server/keyshare/tasks/tasks.go
+++ server/keyshare/tasks/tasks.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/go-errors/errors"
@@ -11,11 +12,12 @@ import (
 	"github.com/privacybydesign/irmago/server/keyshare"
 )
 
-const taskTimeout = 30 * time.Second
+const taskTimeout = 300 * time.Second
 
 type taskHandler struct {
-	conf *Configuration
-	db   keyshare.DB
+	conf           *Configuration
+	db             keyshare.DB
+	revalidateMail bool
 }
 
 func newHandler(conf *Configuration) (*taskHandler, error) {
@@ -31,7 +33,13 @@ func newHandler(conf *Configuration) (*taskHandler, error) {
 		return nil, errors.Errorf("failed to connect to database: %v", err)
 	}
 
-	task := &taskHandler{db: keyshare.DB{DB: db}, conf: conf}
+	keyshareDb := keyshare.DB{DB: db}
+
+	task := &taskHandler{
+		db:             keyshareDb,
+		conf:           conf,
+		revalidateMail: hasMailRevalidation(conf, &keyshareDb)}
+
 	return task, nil
 }
 
@@ -46,6 +54,7 @@ func Do(conf *Configuration) error {
 		"cleanupTokens":   task.cleanupTokens,
 		"cleanupAccounts": task.cleanupAccounts,
 		"expireAccounts":  task.expireAccounts,
+		"revalidateMails": task.revalidateMails,
 	}
 
 	for taskName, taskFunc := range tasks {
@@ -64,6 +73,19 @@ func runWithTimeout(fn func(ctx context.Context)) error {
 
 	fn(ctx)
 	return ctx.Err()
+}
+
+func hasMailRevalidation(conf *Configuration, db *keyshare.DB) bool {
+	ctx, cancel := context.WithTimeout(context.Background(), taskTimeout)
+	defer cancel()
+
+	c, err := db.ExecCountContext(ctx, "SELECT true FROM information_schema.columns where table_schema='irma' AND table_name='emails' AND column_name='revalidate_on'")
+	if err != nil {
+		conf.Logger.WithField("error", err).Error("could not query the schema for column emails.revalidate_on, therefore revalidation is disabled")
+		return false
+	}
+
+	return c != 0
 }
 
 // Remove email addresses marked for deletion long enough ago
@@ -97,12 +119,15 @@ func (t *taskHandler) cleanupAccounts(ctx context.Context) {
 	}
 }
 
+// Send an email to the user informing them their account is expiring in DeleteDelay. If sending is not possible due to
+// a (temporary) invalid e-mail address or network error it is marked for revalidation.
 func (t *taskHandler) sendExpiryEmails(ctx context.Context, id int64, username, lang string) error {
 	// Fetch user's email addresses
-	err := t.db.QueryIterateContext(ctx, "SELECT email FROM irma.emails WHERE user_id = $1",
-		func(emailRes *sql.Rows) error {
+	err := t.db.QueryIterateContext(ctx, "SELECT id, email FROM irma.emails WHERE user_id = $1",
+		func(res *sql.Rows) error {
+			var id int64
 			var email string
-			err := emailRes.Scan(&email)
+			err := res.Scan(&id, &email)
 			if err != nil {
 				return err
 			}
@@ -115,23 +140,56 @@ func (t *taskHandler) sendExpiryEmails(ctx context.Context, id int64, username, 
 				email,
 				lang,
 			)
-			return err
+
+			if t.revalidateMail && err != nil && err != keyshare.ErrNoNetwork {
+				// When email revalidation is enabled and sending was impossible because of
+				// (temporary) MX / A record issues at the domain or an invalid email address,
+				// we mark the record to be revalidated in 5 days from now.
+				if err = t.db.ExecUserContext(ctx, "UPDATE irma.emails SET revalidate_on = $1 WHERE id = $2",
+					time.Now().AddDate(0, 0, 5).Unix(),
+					id); err != nil {
+					t.conf.Logger.WithField("error", err).Error("could not update email address to set revalidate_on")
+					return err
+				}
+				return nil
+			} else {
+				return err
+			}
 		},
 		id,
 	)
 	if err != nil {
-		t.conf.Logger.WithField("error", err).Error("Could not retrieve user's email addresses")
+		t.conf.Logger.WithField("error", err).Error("could not retrieve user's email addresses")
 		return err
 	}
 	return nil
 }
 
-// Mark old unused accounts for deletion, and inform their owners.
+// Mark old unused accounts for deletion, and inform their owners. When email revalidation enabled,
+// email addresses which where marked for revalidation are skipped because these will be processed
+// separately inside revalidateEmails.
 func (t *taskHandler) expireAccounts(ctx context.Context) {
 	// Disable this task when email server is not given
 	if t.conf.EmailServer == "" {
-		t.conf.Logger.Warning("Expiring accounts is disabled, as no email server is configured")
+		t.conf.Logger.Warning("expiring accounts is disabled, as no email server is configured")
 		return
+	}
+
+	query := `
+		SELECT id, username, language
+		FROM irma.users
+		WHERE last_seen < $1 AND (
+			SELECT count(*)
+			FROM irma.emails
+			WHERE irma.users.id = irma.emails.user_id
+			{{revalidate}}
+		) > 0 AND delete_on IS NULL
+		LIMIT 10`
+
+	if t.revalidateMail {
+		query = strings.ReplaceAll(query, "{{revalidate}}", "AND irma.emails.revalidate_on IS NULL")
+	} else {
+		query = strings.ReplaceAll(query, "{{revalidate}}", "")
 	}
 
 	// Iterate over users we have not seen in ExpiryDelay days, and which have a registered email.
@@ -140,15 +198,7 @@ func (t *taskHandler) expireAccounts(ctx context.Context) {
 	// We do this for only 10 users at a time to prevent us from sending out lots of emails
 	// simultaneously, which could lead to our email server being flagged as sending spam.
 	// The users excluded by this limit will get their email next time this task is executed.
-	err := t.db.QueryIterateContext(ctx, `
-		SELECT id, username, language
-		FROM irma.users
-		WHERE last_seen < $1 AND (
-			SELECT count(*)
-			FROM irma.emails
-			WHERE irma.users.id = irma.emails.user_id
-		) > 0 AND delete_on IS NULL
-		LIMIT 10`,
+	err := t.db.QueryIterateContext(ctx, query,
 		func(res *sql.Rows) error {
 			var id int64
 			var username string
@@ -159,28 +209,85 @@ func (t *taskHandler) expireAccounts(ctx context.Context) {
 			}
 
 			// Send emails
-			err = t.sendExpiryEmails(ctx, id, username, lang)
-			// FIXME: 'return nil' will prevent abortion of 'expireAccounts()'
-			// but will not take care of the actual problem of handling the invalid email
-			if err == keyshare.ErrInvalidEmail {
-				return nil
-			}
-			if err != nil {
-				return err // already logged, just abort
+			if err := t.sendExpiryEmails(ctx, id, username, lang); err != nil {
+
+				// To have the exact same behavior as before email revalidation functionality,
+				// we return nil when the error is ErrInvalidEmail
+				if !t.revalidateMail && err == keyshare.ErrInvalidEmail {
+					return nil
+				} else {
+					return err // already logged, just abort
+				}
 			}
 
 			// Finally, do marking for deletion
-			err = t.db.ExecUserContext(ctx, "UPDATE irma.users SET delete_on = $2 WHERE id = $1", id,
-				time.Now().Add(time.Duration(24*t.conf.DeleteDelay)*time.Hour).Unix())
-			if err != nil {
+			if err := t.db.ExecUserContext(ctx, "UPDATE irma.users SET delete_on = $2 WHERE id = $1",
+				id,
+				time.Now().Add(time.Duration(24*t.conf.DeleteDelay)*time.Hour).Unix()); err != nil {
 				return err
 			}
+
 			return nil
 		},
 		time.Now().Add(time.Duration(-24*t.conf.ExpiryDelay)*time.Hour).Unix(),
 	)
 	if err != nil {
-		t.conf.Logger.WithField("error", err).Error("Could not query for accounts that have expired")
+		t.conf.Logger.WithField("error", err).Error("could not query for accounts that have expired")
+		return
+	}
+}
+
+// When email revalidation is enabled, this task revalidates email addresses which where
+// flagged in expireAccounts due to being (temporary) invalid.
+func (t *taskHandler) revalidateMails(ctx context.Context) {
+
+	if !t.revalidateMail {
+		t.conf.Logger.Warning("email address revalidation is disabled because the emails.revalidate_on column is not present in the schema")
+		return
+	}
+
+	// Select only 100 records to prevent a potential storm of DNS requests
+	err := t.db.QueryIterateContext(ctx, `
+		SELECT e.id, e.email
+		FROM irma.emails AS e
+		WHERE e.revalidate_on < $1 
+		LIMIT 100`,
+		func(res *sql.Rows) error {
+			var id int64
+			var email string
+			err := res.Scan(&id, &email)
+			if err != nil {
+				return err
+			}
+
+			addr, err := keyshare.ParseEmailAddress(email)
+
+			if err != nil {
+				return err
+			}
+
+			if err := keyshare.VerifyMXRecord(addr.Host); err != nil {
+				if err == keyshare.ErrNoNetwork {
+					t.conf.Logger.WithField("error", err).Error("could not revalidate email address because there is no active network connection")
+				} else {
+					// When email address still doesn't work, we can assume it's a permanent problem and delete it
+					if _, err = t.db.ExecContext(ctx, "DELETE FROM irma.emails WHERE id = $1", id); err != nil {
+						t.conf.Logger.WithField("error", err).Error("could not delete revalidated and still invalid email address")
+					}
+				}
+			} else {
+				// When email address works again, clear revalidate_on to prevent unwanted deletion
+				if _, err = t.db.ExecContext(ctx, "UPDATE irma.emails SET revalidate_on = NULL WHERE id = $1", id); err != nil {
+					t.conf.Logger.WithField("error", err).Error("could not reset revalidation for email address")
+				}
+			}
+
+			return nil
+		},
+		time.Now().Unix())
+
+	if err != nil {
+		t.conf.Logger.WithField("error", err).Error("could not query email addresses for revalidation")
 		return
 	}
 }
