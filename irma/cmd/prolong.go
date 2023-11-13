9e7ee94925e1d00fadc79f5bb9ad8e07d1e29099
Bob Hageman <bob@schluss.org>
2023-11-13 15:11:02 +0100

    improve: skip prolongation for deprecated issuers

--- irma/cmd/prolong.go
+++ irma/cmd/prolong.go
@@ -16,7 +16,7 @@ import (
 var prolongCmd = &cobra.Command{
 	Use:   "prolong [<path>] [<timestamp>]",
 	Short: "Prolong expiring public and private keys so they are at least valid until the specified timestamp",
-	Long:  "The prolong command updates the latest public and private keys by setting <ExpiryDate> to the specified timestamp for keys which expire before that timestamp.\n\n",
+	Long:  "The prolong command updates the latest public and private keys for non deprecated issuers by setting <ExpiryDate> to the specified timestamp for keys which expire before that timestamp.\n\n",
 	Run: func(cmd *cobra.Command, args []string) {
 
 		if len(args) < 2 {
@@ -40,13 +40,25 @@ var prolongCmd = &cobra.Command{
 		}
 
 		for _, key := range keys {
-			updateExpiryDate(key, timestamp)
+
+			// Is this key part of a deprecated issuer?
+			deprecated, err := deprecated(key, timestamp)
+
+			if err != nil {
+				die("Could not check if key is deprecated", err)
+			}
+
+			if !deprecated {
+				updateExpiryDate(key, timestamp)
+			} else {
+				fmt.Printf("Skipping deprecated key: %s\n", key)
+			}
 		}
 	},
 }
 
 func findHighestKeys(path string) ([]string, error) {
-	var items []string
+	var keys []string
 
 	if err := filepath.Walk(path, func(path string, file os.FileInfo, err error) error {
 		if err != nil {
@@ -78,53 +90,92 @@ func findHighestKeys(path string) ([]string, error) {
 				return err
 			}
 
-			items = append(items, absPath)
-
+			keys = append(keys, absPath)
 		}
 		return nil
 	}); err != nil {
 		return nil, err
 	}
 
-	return items, nil
+	return keys, nil
+}
+
+// Is this key part of a deprecated issuer?
+func deprecated(file string, timestamp int64) (bool, error) {
+
+	descriptionFile := filepath.Dir(file) + "/../description.xml"
+
+	foundTimestamp, err := findTimestamp(descriptionFile, "(<DeprecatedSince>)(.*?)(</DeprecatedSince>)")
+	if err != nil {
+		return false, err
+	}
+
+	if foundTimestamp == 0 || foundTimestamp >= timestamp {
+		// Is not deprecated or does not become deprecated before given timestamp
+		return false, nil
+	}
+
+	return true, nil
 }
 
 func updateExpiryDate(file string, timestamp int64) error {
-	pubKey, err := os.ReadFile(file)
+	foundTimestamp, err := findTimestamp(file, "(<ExpiryDate>)(.*?)(</ExpiryDate>)")
 	if err != nil {
 		return err
 	}
 
-	regex, err := regexp.Compile("(<ExpiryDate>)(.*?)(</ExpiryDate>)")
-	if err != nil {
-		return err
-	}
-
-	matches := regex.FindSubmatch(pubKey)
-	if len(matches) == 0 {
-		return nil
-	}
-
-	currTimestamp, err := strconv.ParseInt(string(matches[2]), 10, 64)
-	if err != nil {
-		return err
-	}
-
-	if currTimestamp >= timestamp {
+	if foundTimestamp >= timestamp {
 		// Does not expire before given timestamp
 		return nil
 	}
 
-	replace := regex.ReplaceAllString(string(pubKey), "${1}"+strconv.FormatInt(timestamp, 10)+"${3}")
-
-	err = os.WriteFile(file, []byte(replace), 0644)
-	if err != nil {
+	if err := replaceTimestamp(file,
+		"<ExpiryDate>"+strconv.FormatInt(foundTimestamp, 10)+"</ExpiryDate>",
+		"<ExpiryDate>"+strconv.FormatInt(timestamp, 10)+"</ExpiryDate>"); err != nil {
 		return err
 	}
 
 	fmt.Printf("Prolonged key: %s\n", file)
 
 	return nil
+}
+
+func replaceTimestamp(file string, find string, replace string) error {
+	bytes, err := os.ReadFile(file)
+	if err != nil {
+		return err
+	}
+
+	result := strings.ReplaceAll(string(bytes), find, replace)
+
+	if err := os.WriteFile(file, []byte(result), 0644); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func findTimestamp(file string, pattern string) (int64, error) {
+	bytes, err := os.ReadFile(file)
+	if err != nil {
+		return 0, err
+	}
+
+	regex, err := regexp.Compile(pattern)
+	if err != nil {
+		return 0, err
+	}
+
+	matches := regex.FindSubmatch(bytes)
+	if len(matches) == 0 {
+		return 0, nil
+	}
+
+	timestamp, err := strconv.ParseInt(string(matches[2]), 10, 64)
+	if err != nil {
+		return 0, err
+	}
+	return timestamp, nil
 }
 
 func init() {
