7e2e6db3b6331aca02a14f6f3a97fdbd626a2f3b
Martijn Kamphuis <m.kamphuis@yivi.app>
2026-06-30 13:39:07 +0200

    adds backwards-compatibility for DID v1.0 First Candidate Recommendation with publicKeyBase58 verification material

--- eudi/did/did.go
+++ eudi/did/did.go
@@ -2,10 +2,12 @@ package did
 
 import (
 	"bytes"
+	"crypto/ed25519"
 	"encoding/json"
 	"fmt"
 
 	"github.com/lestrrat-go/jwx/v3/jwk"
+	"github.com/mr-tron/base58/base58"
 )
 
 // Document represents a W3C DID Document.
@@ -38,9 +40,25 @@ type VerificationMethod struct {
 	ID           string                 `json:"id"`
 	Type         VerificationMethodType `json:"type"`
 	Controller   string                 `json:"controller"`
-	PublicKeyJwk *jwk.Key               `json:"publicKeyJwk,omitempty"`
 	Expires      *string                `json:"expires,omitempty"`
 	Revoked      *string                `json:"revoked,omitempty"`
+	PublicKeyJwk *jwk.Key               `json:"publicKeyJwk,omitempty"`
+
+	PublicKeyMultibase        *string `json:"publicKeyMultibase,omitempty"`
+	decodedMultibasePublicKey *jwk.Key
+
+	PublicKeyBase58        *string `json:"publicKeyBase58,omitempty"`
+	decodedBase58PublicKey *jwk.Key
+}
+
+func (v *VerificationMethod) PublicKey() *jwk.Key {
+	if v.PublicKeyJwk != nil {
+		return v.PublicKeyJwk
+	}
+	if v.decodedMultibasePublicKey != nil {
+		return v.decodedMultibasePublicKey
+	}
+	return v.decodedBase58PublicKey
 }
 
 // VerificationRef can be either a string (reference) or an embedded VerificationMethod.
@@ -139,6 +157,55 @@ func (v *VerificationMethod) UnmarshalJSON(data []byte) error {
 			return fmt.Errorf("failed to parse publicKeyJwk: %v", err)
 		}
 		v.PublicKeyJwk = &key
+	}
+
+	present := 0
+	if v.PublicKeyJwk != nil {
+		present++
+	}
+	if v.PublicKeyMultibase != nil {
+		present++
+	}
+	if v.PublicKeyBase58 != nil {
+		present++
+	}
+	if present > 1 {
+		return fmt.Errorf("publicKeyJwk, publicKeyMultibase and publicKeyBase58 are mutually exclusive")
+	}
+
+	if v.PublicKeyMultibase != nil {
+		rawKey, err := ResolvePublicKeyFromMultibase(*v.PublicKeyMultibase)
+		if err != nil {
+			return fmt.Errorf("failed to decode publicKeyMultibase: %w", err)
+		}
+		key, err := jwk.Import(rawKey)
+		if err != nil {
+			return fmt.Errorf("failed to convert publicKeyMultibase to JWK: %w", err)
+		}
+		v.decodedMultibasePublicKey = &key
+	}
+
+	if v.PublicKeyBase58 != nil {
+		decodedBytes, err := base58.Decode(*v.PublicKeyBase58)
+		if err != nil {
+			return fmt.Errorf("failed to decode publicKeyBase58: %w", err)
+		}
+		switch v.Type {
+		case VerificationMethodType_Ed25519VerificationKey2018:
+			key, err := jwk.Import(ed25519.PublicKey(decodedBytes))
+			if err != nil {
+				return fmt.Errorf("failed to convert publicKeyBase58 to JWK: %w", err)
+			}
+			v.decodedBase58PublicKey = &key
+		case VerificationMethodType_JsonWebKey2020:
+			key, err := jwk.ParseKey(decodedBytes)
+			if err != nil {
+				return fmt.Errorf("failed to parse publicKeyBase58 as JWK: %w", err)
+			}
+			v.decodedBase58PublicKey = &key
+		default:
+			return fmt.Errorf("unsupported verification method type for publicKeyBase58: %s", v.Type)
+		}
 	}
 
 	return nil
--- eudi/did/did_test.go
+++ eudi/did/did_test.go
@@ -1,9 +1,11 @@
 package did
 
 import (
+	"crypto/ed25519"
 	"encoding/json"
 	"testing"
 
+	"github.com/lestrrat-go/jwx/v3/jwk"
 	"github.com/stretchr/testify/require"
 )
 
@@ -107,6 +109,174 @@ func Test_Unmarshal_Document_VerificationRefs_Mixed(t *testing.T) {
 	vm, ok := doc.Authentication[1].(VerificationMethod)
 	require.True(t, ok)
 	require.Equal(t, "did:example:123#key-2", vm.ID)
+}
+
+func Test_Unmarshal_Document_Controller_AsString(t *testing.T) {
+	jsonData := `{
+		"@context": "https://www.w3.org/ns/did/v1",
+		"id": "did:example:123",
+		"controller": "did:example:123"
+	}`
+
+	var doc Document
+	err := json.Unmarshal([]byte(jsonData), &doc)
+	require.NoError(t, err)
+	require.Equal(t, "did:example:123", doc.Controller)
+}
+
+func Test_Unmarshal_Document_Controller_AsArray(t *testing.T) {
+	jsonData := `{
+		"@context": "https://www.w3.org/ns/did/v1",
+		"id": "did:example:123",
+		"controller": ["did:example:123", "did:example:456"]
+	}`
+
+	var doc Document
+	err := json.Unmarshal([]byte(jsonData), &doc)
+	require.NoError(t, err)
+
+	controllers, ok := doc.Controller.([]any)
+	require.True(t, ok, "expected []any, got %T", doc.Controller)
+	require.Equal(t, []any{"did:example:123", "did:example:456"}, controllers)
+}
+
+func Test_Unmarshal_VerificationMethod_WithPublicKeyMultibase_DecodesPublicKey(t *testing.T) {
+	jsonData := `{
+		"id": "did:example:123#key-1",
+		"type": "Multikey",
+		"controller": "did:example:123",
+		"publicKeyMultibase": "z6MkeXCES4onVW4up9Qgz1KRnZsKmGufcaZxF6Zpv2w5QwUK"
+	}`
+
+	var vm VerificationMethod
+	err := json.Unmarshal([]byte(jsonData), &vm)
+	require.NoError(t, err)
+	require.NotNil(t, vm.PublicKeyMultibase)
+
+	require.NotNil(t, vm.PublicKey())
+	var rawKey ed25519.PublicKey
+	require.NoError(t, jwk.Export(*vm.PublicKey(), &rawKey))
+	require.Equal(t, testEd25519PubKey, rawKey)
+}
+
+func Test_Unmarshal_VerificationMethod_WithoutPublicKeyMultibase_PublicKeyIsNil(t *testing.T) {
+	jsonData := `{
+		"id": "did:example:123#key-1",
+		"type": "Multikey",
+		"controller": "did:example:123"
+	}`
+
+	var vm VerificationMethod
+	err := json.Unmarshal([]byte(jsonData), &vm)
+	require.NoError(t, err)
+	require.Nil(t, vm.PublicKey())
+}
+
+func Test_Unmarshal_VerificationMethod_WithPublicKeyJwk_PublicKeyReturnsJwk(t *testing.T) {
+	jsonData := `{
+		"id": "did:example:123#key-1",
+		"type": "JsonWebKey2020",
+		"controller": "did:example:123",
+		"publicKeyJwk": {
+			"kty": "OKP",
+			"crv": "Ed25519",
+			"x": "AQIDBAUGBwgJCgsMDQ4PEBESExQVFhcYGRobHB0eHyA"
+		}
+	}`
+
+	var vm VerificationMethod
+	err := json.Unmarshal([]byte(jsonData), &vm)
+	require.NoError(t, err)
+	require.NotNil(t, vm.PublicKey())
+	require.Equal(t, vm.PublicKeyJwk, vm.PublicKey())
+}
+
+func Test_Unmarshal_VerificationMethod_BothPublicKeys_ReturnsError(t *testing.T) {
+	jsonData := `{
+		"id": "did:example:123#key-1",
+		"type": "Multikey",
+		"controller": "did:example:123",
+		"publicKeyMultibase": "z6MkeXCES4onVW4up9Qgz1KRnZsKmGufcaZxF6Zpv2w5QwUK",
+		"publicKeyJwk": {
+			"kty": "OKP",
+			"crv": "Ed25519",
+			"x": "AQIDBAUGBwgJCgsMDQ4PEBESExQVFhcYGRobHB0eHyA"
+		}
+	}`
+
+	var vm VerificationMethod
+	err := json.Unmarshal([]byte(jsonData), &vm)
+	require.EqualError(t, err, "publicKeyJwk, publicKeyMultibase and publicKeyBase58 are mutually exclusive")
+}
+
+func Test_Unmarshal_VerificationMethod_WithPublicKeyBase58_DecodesPublicKey(t *testing.T) {
+	// publicKeyBase58 is the same as publicKeyMultibase without the leading 'z' header
+	jsonData := `{
+		"id": "did:example:123#key-1",
+		"type": "Ed25519VerificationKey2018",
+		"controller": "did:example:123",
+		"publicKeyBase58": "4wBqpZM9xaSheZzJSMawUKKwhdpChKbZ5eu5ky4Vigw"
+	}`
+
+	var vm VerificationMethod
+	err := json.Unmarshal([]byte(jsonData), &vm)
+	require.NoError(t, err)
+	require.NotNil(t, vm.PublicKeyBase58)
+
+	require.NotNil(t, vm.PublicKey())
+	var rawKey ed25519.PublicKey
+	require.NoError(t, jwk.Export(*vm.PublicKey(), &rawKey))
+	require.Equal(t, testEd25519PubKey, rawKey)
+}
+
+func Test_Unmarshal_VerificationMethod_WithPublicKeyBase58_JsonWebKey2020_DecodesPublicKey(t *testing.T) {
+	// base58 of {"crv":"Ed25519","kty":"OKP","x":"AQIDBAUGBwgJCgsMDQ4PEBESExQVFhcYGRobHB0eHyA"}
+	jsonData := `{
+		"id": "did:example:123#key-1",
+		"type": "JsonWebKey2020",
+		"controller": "did:example:123",
+		"publicKeyBase58": "JdezH6zKGTBMUMsyfkZCVMN66PNviAhqcG4qm4QECqZZ6ct5jbp6GWTLRBgs6XU1D37BhSHkFHvrqbKPFYDRihmpQkpP8qZQgEvfLdHXqMSc"
+	}`
+
+	var vm VerificationMethod
+	err := json.Unmarshal([]byte(jsonData), &vm)
+	require.NoError(t, err)
+	require.NotNil(t, vm.PublicKey())
+
+	var rawKey ed25519.PublicKey
+	require.NoError(t, jwk.Export(*vm.PublicKey(), &rawKey))
+	require.Equal(t, testEd25519PubKey, rawKey)
+}
+
+func Test_Unmarshal_VerificationMethod_WithPublicKeyBase58AndJwk_ReturnsError(t *testing.T) {
+	jsonData := `{
+		"id": "did:example:123#key-1",
+		"type": "Ed25519VerificationKey2018",
+		"controller": "did:example:123",
+		"publicKeyBase58": "4wBqpZM9xaSheZzJSMawUKKwhdpChKbZ5eu5ky4Vigw",
+		"publicKeyJwk": {
+			"kty": "OKP",
+			"crv": "Ed25519",
+			"x": "AQIDBAUGBwgJCgsMDQ4PEBESExQVFhcYGRobHB0eHyA"
+		}
+	}`
+
+	var vm VerificationMethod
+	err := json.Unmarshal([]byte(jsonData), &vm)
+	require.EqualError(t, err, "publicKeyJwk, publicKeyMultibase and publicKeyBase58 are mutually exclusive")
+}
+
+func Test_Unmarshal_VerificationMethod_WithInvalidPublicKeyMultibase_ReturnsError(t *testing.T) {
+	jsonData := `{
+		"id": "did:example:123#key-1",
+		"type": "Multikey",
+		"controller": "did:example:123",
+		"publicKeyMultibase": "zinvalid"
+	}`
+
+	var vm VerificationMethod
+	err := json.Unmarshal([]byte(jsonData), &vm)
+	require.ErrorContains(t, err, "failed to decode publicKeyMultibase")
 }
 
 func Test_Unmarshal_VerificationMethod_Success(t *testing.T) {
